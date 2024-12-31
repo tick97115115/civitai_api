6e6c504dd878ace31bc59f1fbc5c3ec9eb2b800e
Pyon~ Pyon~~ <32931796+tick97115115@users.noreply.github.com>
2024-12-31 10:24:15 +0800

    Comment test_dotenv.py codes

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-print(f"env key value{os.environ['API_KEY']}")
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
+# print(f"env key value{os.environ.get('API_KEY', 'None')}")
